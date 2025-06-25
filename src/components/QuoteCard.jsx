3c81f3dcd83f1ef78bf836801abfb78456e91360
Atul <atul.kevat28@gmail.com>
2025-06-25 12:42:19 +0530

    Add NewTab and QuoteCard components for initial layout

---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function QuoteCard() {
+    return (
+
+        <div className="text-center space-y-2 p-4">
+            <h1 className="text-center text-6xl font-serif text-gray-800">
+                त्र्यंबकं यजामहे सुगन्धिं पुष्टिवर्धनम्। <br />
+                उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात्॥
+            </h1>
+            <p className="text-2xl text-gray-700 italic">
+                We worship the three-eyed one (Shiva), fragrant and nourishing.May He liberate us from death for the sake of immortality, just as a ripe cucumber is freed from the vine.
+            </p>
+            <p className="text-lg text-gray-500">
+                Stay connected to your true Self.
+            </p>
+        </div>
+
+    )
+}
+
+export default QuoteCard;
---
+++ src/pages/NewTab.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import QuoteCard from "../components/QuoteCard";
+
+function NewTab() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <QuoteCard />
+        </div>
+    )
+}
+
+export default NewTab;
