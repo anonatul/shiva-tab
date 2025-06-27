e9a1a12228fd8e4f1d99079b87222f7b64c3457f
Atul <atul.kevat28@gmail.com>
2025-06-27 11:20:13 +0530

    Fetch random quotes from external JSON and handle errors with a fallback quote

--- src/pages/NewTab.jsx
+++ src/pages/NewTab.jsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import QuoteCard from "../components/QuoteCard";
 
-const mockQuote = {
-    sanskrit: "शिवः केवलो गुरुः",
-    english_translation: "Shiva alone is the ultimate teacher.",
-    explanation: "This quote reminds you that all answers lie within yourself."
-}
-
 function NewTab() {
-    const [quote, setQuote] = useState(mockQuote);
+    const [quote, setQuote] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://raw.githubusercontent.com/anonatul/shiva-tab/main/public/quotes.json');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`)
+                }
+                const result = await response.json();
+                const randomIdx = Math.floor(Math.random() * result.length);
+                setQuote(result[randomIdx]);
+            } catch (error) {
+                console.log(error);
+                setQuote({
+                    sanskrit: "उपदेशो हि मूर्खाणां प्रकोपाय न शान्तये।",
+                    english_translation: "Teaching the ignorant leads only to their irritation, not peace.",
+                    explanation: "Sometimes it’s best to let life teach rather than speak."
+                });
+            }
+        }
+
+        fetchData();
+    }, []);
+
+
     return (
         <div className="flex justify-center items-center h-screen">
-            <QuoteCard quote={quote} />
+            {quote ? <QuoteCard quote={quote} /> : ""}
         </div>
     )
 }
