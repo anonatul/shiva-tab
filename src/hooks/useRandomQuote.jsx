e7ba0b2b985aae5fbf0084d9f5a0f2e5a8359d77
Atul <atul.kevat28@gmail.com>
2025-06-27 18:06:57 +0530

    Refactor NewTab component to utilize useRandomQuote hook for fetching quotes

---
+++ src/hooks/useRandomQuote.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function useRandomQuote() {
+    const [quote, setQuote] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://raw.githubusercontent.com/anonatul/shiva-tab/main/public/quotes.json');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`)
+                }
+                const result = await response.json();
+                const randomIdx = Math.floor(Math.random() * result.length);
+                setQuote(result[randomIdx]);
+            } catch (error) {
+                console.log(error);
+                setQuote({
+                    sanskrit: "उपदेशो हि मूर्खाणां प्रकोपाय न शान्तये।",
+                    english_translation: "Teaching the ignorant leads only to their irritation, not peace.",
+                    explanation: "Sometimes it’s best to let life teach rather than speak."
+                });
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    return quote;
+}
--- src/pages/NewTab.jsx
+++ src/pages/NewTab.jsx
@@ -1,33 +1,11 @@
 import { useEffect, useState } from "react";
 import QuoteCard from "../components/QuoteCard";
 import ActionButtons from "../components/ActionButtons";
+import useRandomQuote from "../hooks/useRandomQuote";
 
 function NewTab() {
-    const [quote, setQuote] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://raw.githubusercontent.com/anonatul/shiva-tab/main/public/quotes.json');
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`)
-                }
-                const result = await response.json();
-                const randomIdx = Math.floor(Math.random() * result.length);
-                setQuote(result[randomIdx]);
-            } catch (error) {
-                console.log(error);
-                setQuote({
-                    sanskrit: "उपदेशो हि मूर्खाणां प्रकोपाय न शान्तये।",
-                    english_translation: "Teaching the ignorant leads only to their irritation, not peace.",
-                    explanation: "Sometimes it’s best to let life teach rather than speak."
-                });
-            }
-        }
-
-        fetchData();
-    }, []);
-
+    const quote = useRandomQuote();
 
     return (
         <div className="flex flex-col justify-center items-center h-screen animate-fade-in duration-500">
